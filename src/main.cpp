9bef67f2342f641f6ec5524c2a561dd9c086f876
Hanzeel Villa <hanzeelvilla0@gmail.com>
2026-04-13 13:34:43 -0600

    Matrix display test

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,31 @@
 #include "SpotifyClient.h"
 #include <Arduino.h>
 #include <WiFi.h>
+#include <MD_Parola.h>
+#include <MD_MAX72xx.h>
+#include <SPI.h>
 
 SpotifyClient spotify(SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET, SPOTIFY_REFRESH_TOKEN);
+MD_Parola P = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
 
 void setupWifi();
+void printTrackInfo(const TrackInfo& track);
 
 void setup() {
   Serial.begin(115200);
+
+  P.begin();
+  P.setIntensity(5); // Set brightness (0-15)
+  P.displayClear();
+
+  P.displayText("HELLO WORLD", PA_CENTER, 0, 0, PA_PRINT, PA_NO_EFFECT);
+
   setupWifi();
 }
 
 void loop() {
+  P.displayAnimate();
+
   if (WiFi.status() != WL_CONNECTED) {
     Serial.println("WiFi disconnected, reconnecting...");
     setupWifi();
@@ -28,11 +42,7 @@ void loop() {
       TrackInfo track = spotify.getCurrentTrack();
 
       if (track.isPlaying) {
-        Serial.println("\n==========================");
-        Serial.printf("Now Playing: %s\n", track.name.c_str());
-        Serial.printf("Artist(s):   %s\n", track.artist.c_str());
-        Serial.printf("Album:       %s\n", track.album.c_str());
-        Serial.println("==========================");
+        printTrackInfo(track);
       } else {
         Serial.println("[Info] No active track found on Spotify.");
       }
@@ -53,4 +63,12 @@ void setupWifi() {
 
   Serial.println("");
   Serial.println("WiFi connected");
+}
+
+void printTrackInfo(const TrackInfo& track) {
+  Serial.println("\n==========================");
+  Serial.printf("Now Playing: %s\n", track.name.c_str());
+  Serial.printf("Artist(s):   %s\n", track.artist.c_str());
+  Serial.printf("Album:       %s\n", track.album.c_str());
+  Serial.println("==========================");
 }
